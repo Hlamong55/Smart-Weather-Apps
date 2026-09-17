459a89ec2c78c3289991fc2575238265226e303b
Hlamong55 <bijoymarma55@gmail.com>
2026-09-17 23:54:48 +0600

    Organized the main page layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,24 @@ import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Home from './pages/Home';
 import About from './pages/About';
+import MainLayout from './layouts/MainLayout';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home></Home>,
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        element: <Home></Home>
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+    ]
   },
-  {
-    path: "/about",
-    element: <About></About>,
-  },
+
 ]);
 
 function Router() {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@
 const Home = () => {
     return (
         <div>
-            Hello world, from Home!!
+            <h1 className="text-6xl font-black text-blue-300">NextLevel <span className="text-blue-600">Weather</span></h1>
         </div>
     );
 };
