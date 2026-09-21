c432832448d5098ebd4f8b7be1e914d1d43abdec
Hlamong55 <bijoymarma55@gmail.com>
2026-09-21 23:55:34 +0600

    Changes updated in modal formation

--- src/components/LocationModal.jsx
+++ src/components/LocationModal.jsx
@@ -1,16 +1,30 @@
-import { X } from 'lucide-react';
+import { X } from "lucide-react";
 
-const LocationModal = ( {onClose} ) => {
+const LocationModal = ({ onClose }) => {
   return (
     <div className="fixed inset-0 flex justify-center items-center bg-gray-600/70">
-      <div className="h-85 w-md bg-gray-200 shadow-2xl rounded-2xl flex items-start justify-between p-4">
-        <h2 className="text-2xl font-bold">Where are you today?</h2>
+      <div className="h-85 w-md bg-gray-200 shadow-2xl rounded-2xl p-4">
 
-        <button 
-        onClick={onClose}
-        className='bg-gray-300 text-red-600 rounded-full p-1 hover:bg-red-600 hover:text-white'>
+        <div className="flex justify-between items-center"> 
+          <h2 className="text-2xl font-bold">Where are you today?</h2>
+
+          <button
+            onClick={onClose}
+            className="bg-gray-300 text-red-600 rounded-full border p-1 hover:bg-red-600 hover:text-white"
+          >
             <X />
-        </button>
+          </button>
+        </div>
+
+        <div>
+          <form>
+            <input
+              placeholder="Enter City Name"
+              type="text"
+              className="w-full border"
+            />
+          </form>
+        </div>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,29 +2,34 @@ import { useState } from "react";
 import LocationModal from "../components/LocationModal";
 
 const Home = () => {
-    const [click, setClick] = useState(false)
-    console.log(click);
+  const [click, setClick] = useState(false);
+  console.log(click);
 
-    return (
-        <div className="text-center">
-            <h1 className="text-6xl font-black text-blue-300">NextLevel <span className="text-blue-600">Weather</span></h1>
+  return (
+    <div>
+      <div className="text-center">
+        <h1 className="text-6xl font-black text-blue-300">
+          NextLevel <span className="text-blue-600">Weather</span>
+        </h1>
 
-            <p className="text-lg font-semibold py-2 text-gray-500">Check your todays Weather in NextLevel</p>
+        <p className="text-lg font-semibold py-2 text-gray-500">
+          Check your todays Weather in NextLevel
+        </p>
+      </div>
 
-            <div className="pt-5">
-                <button 
-                type="button" 
-                onClick={()=>setClick("Checking")}
-                className="text-lg font-medium px-5 py-2.5 bg-blue-600 rounded-2xl text-white hover:scale-105 transition-all hover:bg-blue-800">
-                    Check Weather
-                </button>
-            </div>
+      <div className="pt-5 flex justify-center">
+        <button
+          type="button"
+          onClick={() => setClick("Checking")}
+          className="text-lg font-medium px-5 py-2.5 bg-blue-600 rounded-2xl text-white hover:scale-105 transition-all hover:bg-blue-800"
+        >
+          Check Weather
+        </button>
+      </div>
 
-            {
-                click && <LocationModal onClose={()=>setClick(false)}></LocationModal>
-            }
-        </div>
-    );
+      {click && <LocationModal onClose={() => setClick(false)}></LocationModal>}
+    </div>
+  );
 };
 
 export default Home;
